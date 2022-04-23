fccd56e93ad07f1b0f68199710b671cc080f4b20
Abduqodir Bozorov <89505725+abduqodirBozorov@users.noreply.github.com>
2022-04-23 10:09:29 +0000

    final

--- index.js
+++ index.js
@@ -50,545 +50,6 @@ var insts = [{
       gr_k: 7,
       con_k: 8,
       olimp: 0
-    },
-    {
-      mvdir: 5510100048.0,
-      nomi: "Davolash ishi (Qarshi shahri)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100053.0,
-      nomi: "Davolash ishi (Kattaqo`rg`on shahri)",
-      gr_k: 3,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100054.0,
-      nomi: "Davolash ishi (Samarqand shahri)",
-      gr_k: 8,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100182.0,
-      nomi: "Davolash ishi (Chiroqchi tumani)",
-      gr_k: 4,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100183.0,
-      nomi: "Davolash ishi (Dehqonobod tumani)",
-      gr_k: 4,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100184.0,
-      nomi: "Davolash ishi (G`uzor tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100185.0,
-      nomi: "Davolash ishi (Kasbi tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100186.0,
-      nomi: "Davolash ishi (Kitob tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100187.0,
-      nomi: "Davolash ishi (Koson tumani)",
-      gr_k: 3,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100188.0,
-      nomi: "Davolash ishi (Mirishkor tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100190.0,
-      nomi: "Davolash ishi (Nishon tumani)",
-      gr_k: 3,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100191.0,
-      nomi: "Davolash ishi (Qamashi tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100192.0,
-      nomi: "Davolash ishi (Qarshi tumani)",
-      gr_k: 3,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100194.0,
-      nomi: "Davolash ishi (Yakkabog` tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100215.0,
-      nomi: "Davolash ishi (Bulung`ur tumani)",
-      gr_k: 3,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100216.0,
-      nomi: "Davolash ishi (Ishtixon tumani)",
-      gr_k: 7,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100217.0,
-      nomi: "Davolash ishi (Jomboy tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100218.0,
-      nomi: "Davolash ishi (Kattaqo`rg`on tumani)",
-      gr_k: 4,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100219.0,
-      nomi: "Davolash ishi (Narpay tumani)",
-      gr_k: 6,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100220.0,
-      nomi: "Davolash ishi (Nurobod tumani)",
-      gr_k: 8,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100221.0,
-      nomi: "Davolash ishi (Oqdaryo tumani)",
-      gr_k: 4,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100222.0,
-      nomi: "Davolash ishi (Pastdarg`om tumani)",
-      gr_k: 4,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100223.0,
-      nomi: "Davolash ishi (Paxtachi tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100224.0,
-      nomi: "Davolash ishi (Payariq tumani)",
-      gr_k: 4,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100225.0,
-      nomi: "Davolash ishi (Qo`shrabot tumani)",
-      gr_k: 5,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100226.0,
-      nomi: "Davolash ishi (Samarqand tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100227.0,
-      nomi: "Davolash ishi (Tayloq tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100228.0,
-      nomi: "Davolash ishi (Urgut tumani)",
-      gr_k: 3,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510100253.0,
-      nomi: "Davolash ishi (Shahrisabz shahri)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200047.0,
-      nomi: "Pediatriya ishi (Termiz shahri)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200048.0,
-      nomi: "Pediatriya ishi (Qarshi shahri)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200051.0,
-      nomi: "Pediatriya ishi (Navoiy shahri)",
-      gr_k: 4,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200052.0,
-      nomi: "Pediatriya ishi (Zarafshon shahri)",
-      gr_k: 3,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200054.0,
-      nomi: "Pediatriya ishi (Samarqand shahri)",
-      gr_k: 6,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200064.0,
-      nomi: "Pediatriya ishi (G`ozg`on shahri)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200171.0,
-      nomi: "Pediatriya ishi (Denov tumani)",
-      gr_k: 3,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200174.0,
-      nomi: "Pediatriya ishi (Oltinsoy tumani)",
-      gr_k: 3,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200175.0,
-      nomi: "Pediatriya ishi (Qiziriq tumani)",
-      gr_k: 3,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200182.0,
-      nomi: "Pediatriya ishi (Chiroqchi tumani)",
-      gr_k: 6,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200184.0,
-      nomi: "Pediatriya ishi (G`uzor tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200185.0,
-      nomi: "Pediatriya ishi (Kasbi tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200186.0,
-      nomi: "Pediatriya ishi (Kitob tumani)",
-      gr_k: 4,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200187.0,
-      nomi: "Pediatriya ishi (Koson tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200189.0,
-      nomi: "Pediatriya ishi (Muborak tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200190.0,
-      nomi: "Pediatriya ishi (Nishon tumani)",
-      gr_k: 4,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200192.0,
-      nomi: "Pediatriya ishi (Qarshi tumani)",
-      gr_k: 4,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200193.0,
-      nomi: "Pediatriya ishi (Shahrisabz tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200207.0,
-      nomi: "Pediatriya ishi (Karmana tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200209.0,
-      nomi: "Pediatriya ishi (Navbahor tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200210.0,
-      nomi: "Pediatriya ishi (Nurota tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200211.0,
-      nomi: "Pediatriya ishi (Qiziltepa tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200214.0,
-      nomi: "Pediatriya ishi (Xatirchi tumani)",
-      gr_k: 3,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200216.0,
-      nomi: "Pediatriya ishi (Ishtixon tumani)",
-      gr_k: 3,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200218.0,
-      nomi: "Pediatriya ishi (Kattaqo`rg`on tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200220.0,
-      nomi: "Pediatriya ishi (Nurobod tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200224.0,
-      nomi: "Pediatriya ishi (Payariq tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200225.0,
-      nomi: "Pediatriya ishi (Qo`shrabot tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200253.0,
-      nomi: "Pediatriya ishi (Shahrisabz shahri)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510200255.0,
-      nomi: "Pediatriya ishi (Bandixon tumani)",
-      gr_k: 2,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300182.0,
-      nomi: "Tibbiy profilaktika ishi (Chiroqchi tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300184.0,
-      nomi: "Tibbiy profilaktika ishi (G`uzor tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300185.0,
-      nomi: "Tibbiy profilaktika ishi (Kasbi tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300186.0,
-      nomi: "Tibbiy profilaktika ishi (Kitob tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300191.0,
-      nomi: "Tibbiy profilaktika ishi (Qamashi tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300193.0,
-      nomi: "Tibbiy profilaktika ishi (Shahrisabz tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300194.0,
-      nomi: "Tibbiy profilaktika ishi (Yakkabog` tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300218.0,
-      nomi: "Tibbiy profilaktika ishi (Kattaqo`rg`on tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300219.0,
-      nomi: "Tibbiy profilaktika ishi (Narpay tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300220.0,
-      nomi: "Tibbiy profilaktika ishi (Nurobod tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300221.0,
-      nomi: "Tibbiy profilaktika ishi (Oqdaryo tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300223.0,
-      nomi: "Tibbiy profilaktika ishi (Paxtachi tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300225.0,
-      nomi: "Tibbiy profilaktika ishi (Qo`shrabot tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300228.0,
-      nomi: "Tibbiy profilaktika ishi (Urgut tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510300253.0,
-      nomi: "Tibbiy profilaktika ishi (Shahrisabz shahri)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510900224.0,
-      nomi: "Tibbiy-biologik ish (Payariq tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510900225.0,
-      nomi: "Tibbiy-biologik ish (Qo`shrabot tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
-    },
-    {
-      mvdir: 5510900226.0,
-      nomi: "Tibbiy-biologik ish (Samarqand tumani)",
-      gr_k: 1,
-      con_k: 0,
-      olimp: 0
     }
   ],
 }, {
@@ -780,8 +241,14 @@ var samduYunalish = []
 var sammiYunalish = []
 var vetYunalish = []
 
-var exit = "❌Bekor qilish"
-
+// commands
+var commands = {
+  exit:"❌Bekor qilish",
+  register:"📃 Ro'yxatdan o'tish",
+  check:"🔍 Tekshirish",
+  info:"📖 Ma'lumot",
+  sendMessage:"📨 Xabar qoldirish",
+}
 
 
 const chunk = (arr, size) =>
@@ -790,13 +257,12 @@ const chunk = (arr, size) =>
   );
 
 
-
 for (let i = 0; i < insts.length; i++) {
   const otm = insts[i];
   otms.push(otm.nomi)
 }
 
-otms.push(exit)
+otms.push(commands.exit)
 var OTMS = chunk(otms, 3)
 
 
@@ -804,22 +270,26 @@ for (let i = 0; i < insts[0].yunalishlar.length; i++) {
   const sammiItem = insts[0].yunalishlar[i];
   sammiYunalish.push(sammiItem.nomi)
 }
-sammiYunalish.push(exit)
+sammiYunalish.push(commands.exit)
+
 
 for (let i = 0; i < insts[1].yunalishlar.length; i++) {
   const samduItem = insts[1].yunalishlar[i];
   samduYunalish.push(samduItem.nomi)
 }
-samduYunalish.push(exit)
+samduYunalish.push(commands.exit)
+
 
 for (let i = 0; i < insts[2].yunalishlar.length; i++) {
   const vetItem = insts[2].yunalishlar[i];
   vetYunalish.push(vetItem.nomi)
 }
-vetYunalish.push(exit)
+vetYunalish.push(commands.exit)
 
 
-
+var samDuYunalish = chunk(samduYunalish, 2)
+var samMiYunalish = chunk(sammiYunalish, 2)
+var veTYunalish = chunk(vetYunalish, 2)
 
 
 
@@ -854,19 +324,13 @@ bot.onText(/\/start/, (msg) => {
 })
 
 bot.on('message', (msg) => {
-  var reg = "📃 Ro'yxatdan o'tish";
-  if (msg.text.toString().indexOf(reg) === 0) {
+  if (msg.text.toString().indexOf(commands.register) === 0) {
       bot.sendMessage(msg.chat.id, "Oliy ta'lim muassasasini tanlang", 
       {"reply_markup": {"keyboard": OTMS}
     })
   }
 });
 
-
-
-var samDuYunalish = chunk(samduYunalish, 2)
-var samMiYunalish = chunk(sammiYunalish, 2)
-var veTYunalish = chunk(vetYunalish, 2)
 
 
 bot.on('message', (msg) => {
@@ -902,8 +366,7 @@ bot.on('message', (msg) => {
 
 
 bot.on('message', (msg) => {
-  if (msg.text.toString().indexOf(exit) === 0) {
-    userData = {}
+  if (msg.text.toString().indexOf(commands.exit) === 0) {
       bot.sendMessage(msg.chat.id, "🏘 Asosiy menu", {
       "reply_markup": {"keyboard": [["📃 Ro'yxatdan o'tish", "🔍 Tekshirish"], ["📖 Ma'lumot"], ["📨 Xabar qoldirish"]]}
     })
@@ -928,9 +391,10 @@ bot.on('message', (msg) => {
   
   if (msg.text.toString() === elim.nomi) {
     userData.yunalish = elim
-
-    var jsomim = JSON.stringify(userData);
-    console.log(jsomim)
+    bot.sendMessage(msg.chat.id, "Ismingizni ", {
+      "reply_markup": {"keyboard": [["📃 Ro'yxatdan o'tish", "🔍 Tekshirish"], ["📖 Ma'lumot"], ["📨 Xabar qoldirish"]]}
+    })
   }
 }
 });
+
